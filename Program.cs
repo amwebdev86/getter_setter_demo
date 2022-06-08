154a85c0653535e18dbdb7e6e0de57f8967549d3
Auroiah Morgan <amwebdev@outlook.com>
2022-06-08 18:40:59 -0500

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -10,7 +10,13 @@ namespace ConsoleApp1
         {
             // instantiate my object to create a point in memory and hold a ref to it.
             MyObject myObject = new MyObject();
+
+            //set the Health of myObject
+            // set the _health private variable
             myObject.Health = 16;
+
+            //Get the Health of my object 
+            // references _health private variable 
             Console.WriteLine($"{myObject.Health}");
 
         }
